20707932671b2e669ff8cf309839730824f36555
Shahrizoda <saidovashaxnoza092@gmail.com>
2026-02-04 19:35:28 +0500

    create simple chatbot

---
+++ bot.py
@@ -0,0 +1,36 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+TOKEN = "8394119459:AAFd3ZWHgBRRX9Y7CobcDj2LOA4tGnPedlI"
+
+ADMIN_ID = 7213756755
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(
+        "Salom 👋\nBu bot orqali menga anonim xabar yuborishingiz mumkin.\n"
+        "Shunchaki xabaringizni yozing ✍️"               
+                         )
+@dp.message()
+async def forward_to_admin(message: Message):
+    text = (
+        "📩 Yangi xabar:\n\n"
+        f"👤 From: @{message.from_user.username}\n"
+        f"🆔 ID: {message.from_user.id}\n\n"
+        f"{message.text}"
+    )
+    await bot.send_message(ADMIN_ID, text)
+    await message.answer("Xabaringiz yuborildi! Rahmat 😊")
+    
+async def main():
+    await dp.start_polling(bot)
+    
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
